b2a2c72ce5f6579e1f3b62fc705e323be26634fb
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-11-18 17:34:40 +0530

    basic logic completed

--- BMI Calculator/index.js
+++ BMI Calculator/index.js
@@ -4,26 +4,24 @@ const button = document.querySelector(".button");
 const outputBox = document.querySelector("#output");
 
 function updateUI(output) {
-  output.innerHTML = output;
+  outputBox.innerHTML = output;
 }
 
 const inputHandler = () => {
   let height = Number(heightInput.value);
   let weight = Number(weightInput.value);
 
+  if (weight == "") {
+    updateUI("Enter weight");
+    return;
+  }
   if (height == "") {
     updateUI("Enter height");
     return;
   }
 
-  if (weight == "") {
-    updateUI("Enter weight");
-    return;
-  }
-
   const bmi = weight / (height * height);
-  console.log(bmi);
-  updateUI(bmi);
+  updateUI(bmi.toFixed(2));
 };
 
 button.addEventListener("click", inputHandler);
