449298eac9c7d183725bb67a5954f2cd4de69a65
MaheshRautrao <101188065+MaheshRautrao@users.noreply.github.com>
2023-11-21 20:11:01 +0530

    edge cases and input checks added and app completed

--- BMI Calculator/index.js
+++ BMI Calculator/index.js
@@ -17,31 +17,42 @@ const inputHandler = () => {
   let weight = Number(weightInput.value);
 
   if (weight == "") {
-    updateUI("Enter weight");
+    updateUI("Enter weight", "invalid data");
     return;
   }
   if (height == "") {
-    updateUI("Enter height");
+    updateUI("Enter height", "invalid data");
+    return;
+  }
+  if (weight < 1 || weight > 600) {
+    updateUI(
+      "Enter valid weight, max weight:600 , min weight: 1",
+      "invalid data"
+    );
+    return;
+  }
+  if (height < 1 || height > 3) {
+    updateUI(
+      "Enter valid height, max height:3 , min height: 1",
+      "invalid data"
+    );
     return;
   }
 
   const bmi = weight / (height * height);
-  const bmiIndex = bmi.toFixed(2);
+  const bmiIndex = Number(bmi.toFixed(2));
   let category;
 
-  switch (bmiIndex) {
-    case bmiIndex < 18.5:
-      category = "Underweight";
-      break;
-    case bmiIndex >= 18.5 || bmiIndex <= 24.9:
-      category = "Healthy weight";
-      break;
-    case bmiIndex >= 25.0 || bmiIndex <= 29.9:
-      category = "Overweight";
-      break;
-    default:
-      category = "Obesity";
+  if (bmiIndex < 18.5) {
+    category = "Underweight";
+  } else if (bmiIndex >= 18.5 && bmiIndex <= 24.9) {
+    category = "Healthy weight";
+  } else if (bmiIndex >= 25.0 && bmiIndex <= 29.9) {
+    category = "Overweight";
+  } else {
+    category = "Obesity";
   }
+
   updateUI(bmiIndex, category);
 };
 
